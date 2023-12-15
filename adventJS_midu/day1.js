e1d56739dcd9b3b6df1a299674251d1c4611a8fe
Omar Anaya <omaranaya616@gmail.com>
2023-12-15 17:16:14 -0600

    Cambios de los retos de advent_js MIDU

--- adventJS_midu/day1.js
+++ adventJS_midu/day1.js
@@ -6,7 +6,7 @@
 
 // En otras palabras, si hay más de un número repetido, debes devolver el número cuya segunda ocurrencia aparezca primero en la lista. Si no hay números repetidos, devuelve -1.
 
-function findFirstRepeated(gifts){
+function findFirstRepeated_old(gifts){
   
   let repeated = [];
   let firstRepeated = [];
@@ -29,6 +29,33 @@ function findFirstRepeated(gifts){
   return firstRepeated.length == 0 ? -1 : firstRepeated[0];
 }
 
+const giftIds_old = [2, 1, 3, 5, 3, 2];
+const firstRepeatedId_old = findFirstRepeated_old(giftIds_old);
+// console.log(firstRepeatedId_old);
+
+function findFirstRepeated(gifts){
+  
+  // let repeatedIndex = [];
+  // gifts.reduce( (accum, elem, i, arrG) => {
+
+  //   let giftArr = arrG.slice(i+1);
+  //   repeatedIndex.push( giftArr.findIndex( val => elem === val ) );
+  //   // repeatedIndex.push( giftArr.findIndex( val => elem === val ) + (i+1) );
+
+  //   return elem;
+  // }, gifts[0]);
+
+  let repeatedIndex = gifts.filter( (elem, i, arrG) => {
+
+    // let giftArr = arrG.slice(i+1);
+    let index = arrG.slice(i+1).findIndex( val => elem === val );
+console.log(index);
+    return index;
+  });
+
+  console.log(repeatedIndex);
+}
+
 const giftIds = [2, 1, 3, 5, 3, 2];
 const firstRepeatedId = findFirstRepeated(giftIds);
-console.log(firstRepeatedId);
+// console.log(firstRepeatedId);
