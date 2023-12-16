9a4147da55a4f41c501bc66b93ed041dca2f1472
Omar Anaya <omaranaya616@gmail.com>
2023-12-15 18:33:43 -0600

    Cambios a desarrollos de Advent of JS MIDU

--- adventJS_midu/day1.js
+++ adventJS_midu/day1.js
@@ -6,56 +6,24 @@
 
 // En otras palabras, si hay más de un número repetido, debes devolver el número cuya segunda ocurrencia aparezca primero en la lista. Si no hay números repetidos, devuelve -1.
 
-function findFirstRepeated_old(gifts){
-  
-  let repeated = [];
-  let firstRepeated = [];
-  for(let i=0; i<gifts.length; i++){
-    
-    firstRepeated = repeated.filter( elem => {
-      if(gifts[i] === elem)
-        return elem;
-    });
-
-    if(firstRepeated.length > 0)
-      break;
-    
-    for(let j=(i+1); j<gifts.length; j++){
-        if(gifts[i] === gifts[j])
-          repeated.push( gifts[i] );
-    }
-  }
-  	
-  return firstRepeated.length == 0 ? -1 : firstRepeated[0];
-}
-
-const giftIds_old = [2, 1, 3, 5, 3, 2];
-const firstRepeatedId_old = findFirstRepeated_old(giftIds_old);
-// console.log(firstRepeatedId_old);
-
 function findFirstRepeated(gifts){
   
-  // let repeatedIndex = [];
-  // gifts.reduce( (accum, elem, i, arrG) => {
+  let arrG = gifts;
 
-  //   let giftArr = arrG.slice(i+1);
-  //   repeatedIndex.push( giftArr.findIndex( val => elem === val ) );
-  //   // repeatedIndex.push( giftArr.findIndex( val => elem === val ) + (i+1) );
+  let repeatedIndex = [];
+  gifts.forEach( (elem, i) => {
 
-  //   return elem;
-  // }, gifts[0]);
-
-  let repeatedIndex = gifts.filter( (elem, i, arrG) => {
-
-    // let giftArr = arrG.slice(i+1);
     let index = arrG.slice(i+1).findIndex( val => elem === val );
-console.log(index);
-    return index;
-  });
 
-  console.log(repeatedIndex);
+    if(index >= 0)
+      repeatedIndex.push( index + (i+1) );
+  });
+  let lowestIndex = repeatedIndex.sort()[0];
+
+  return lowestIndex != undefined ? gifts[lowestIndex] : -1;
 }
 
 const giftIds = [2, 1, 3, 5, 3, 2];
+// const giftIds = [2, 4, 3, 5, 1];
 const firstRepeatedId = findFirstRepeated(giftIds);
-// console.log(firstRepeatedId);
+console.log(firstRepeatedId);
--- adventJS_midu/day3.js
+++ adventJS_midu/day3.js
@@ -4,31 +4,65 @@
 
 // Tu tarea es escribir una función que identifique y devuelva el primer paso extra que se ha añadido o eliminado en la cadena de fabricación. Si no hay ninguna diferencia entre las secuencias, devuelve una cadena vacía.
 
+// function findNaughtyStep_old(original, modified) {
+//    // Code here
+   
+//    original = original.split('');
+//    modified = modified.split('');
+   
+//    let mayorArray = [];
+//    let minorArray = [];
+//    if(original.length < modified.length){
+//      mayorArray = modified;
+//      minorArray = original;
+//    } else {
+//      mayorArray = original;
+//      minorArray = modified;
+//    }
+   
+//    let extraDataFilter = mayorArray.filter( elem => {
+//      if(!minorArray.some( val => elem === val ))
+//        return elem;
+//    });
+ 
+//    return extraDataFilter[0] != undefined ? extraDataFilter[0] : '';
+//  }
+ 
+//  const original_old = 'abcd'
+//  const modified_old = 'abcde'
+ 
+// console.time();
+// let result_old = findNaughtyStep_old(original_old, modified_old) // 'e'
+// console.log( result_old );
+// console.timeEnd();
+
+
 function findNaughtyStep(original, modified) {
-   // Code here
-   original = original.split('');
-   modified = modified.split('');
-   
-   let mayorArray = [];
-   let minorArray = [];
-   if(original.length < modified.length){
-     mayorArray = modified;
-     minorArray = original;
-   } else {
-     mayorArray = original;
-     minorArray = modified;
-   }
-   
-   let extraDataFilter = mayorArray.filter( elem => {
-     if(!minorArray.some( val => elem === val ))
-       return elem;
-   });
- 
-   return extraDataFilter[0] != undefined ? extraDataFilter[0] : '';
- }
- 
- const original = 'abcd'
- const modified = 'abcde'
- 
- let result = findNaughtyStep(original, modified) // 'e'
- console.log( result );
+  // Code here
+
+  let   ;
+  let copyModified;
+  if(original.length < modified.length){
+    copyOriginal = modified;
+    copyModified = original;
+  }
+
+  let nonAllowedStep;
+  for(let i=0; i<copyOriginal.length; i++){
+  
+    let result = copyModified.split('').some( elem => elem === copyOriginal[i] );
+
+    if(!result)
+      nonAllowedStep = copyOriginal[i];
+  }
+  
+  return nonAllowedStep != undefined ? nonAllowedStep : '';
+}
+
+const original = 'abcd'
+const modified = 'abcde'
+
+console.time();
+let result = findNaughtyStep(original, modified) // 'e'
+console.log( result );
+console.timeEnd();
